a07c91b356e1831b9e6b91d4172fa116e3b9c349
Fiasko <snakeice2000@mail.ru>
2020-10-21 14:28:58 +0300

    upd

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace LabWeb_4_session_1
+{
+    partial class AuthForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Auth = new System.Windows.Forms.Button();
+            this.login = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelWelcome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Auth
+            // 
+            this.Auth.Location = new System.Drawing.Point(273, 257);
+            this.Auth.Name = "Auth";
+            this.Auth.Size = new System.Drawing.Size(134, 59);
+            this.Auth.TabIndex = 0;
+            this.Auth.Text = "Вход";
+            this.Auth.UseVisualStyleBackColor = true;
+            this.Auth.Click += new System.EventHandler(this.Auth_Click);
+            // 
+            // login
+            // 
+            this.login.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login.Location = new System.Drawing.Point(46, 141);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(623, 29);
+            this.login.TabIndex = 1;
+            // 
+            // password
+            // 
+            this.password.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(46, 204);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(623, 29);
+            this.password.TabIndex = 2;
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(49, 122);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(82, 13);
+            this.labelLogin.TabIndex = 3;
+            this.labelLogin.Text = "Введите почту:";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(46, 185);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(91, 13);
+            this.labelPassword.TabIndex = 4;
+            this.labelPassword.Text = "Введите пароль:";
+            // 
+            // labelWelcome
+            // 
+            this.labelWelcome.AutoSize = true;
+            this.labelWelcome.Font = new System.Drawing.Font("Segoe UI Black", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWelcome.Location = new System.Drawing.Point(92, 32);
+            this.labelWelcome.Name = "labelWelcome";
+            this.labelWelcome.Size = new System.Drawing.Size(517, 65);
+            this.labelWelcome.TabIndex = 5;
+            this.labelWelcome.Text = "Добро пожаловать!";
+            // 
+            // AuthForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 346);
+            this.Controls.Add(this.labelWelcome);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.Auth);
+            this.Name = "AuthForm";
+            this.Text = "Авторизация";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Auth;
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelWelcome;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace LabWeb_4_session_1
+{
+    public partial class AuthForm : Form {
+        readonly string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=session1_rasulov;";
+        string query;
+        public AuthForm() {
+            InitializeComponent();
+        }
+
+        private void Auth_Click(object sender, EventArgs e) {
+            // Change the username, password and database according to your needs
+            // You can ignore the database option if you want to access all of them.
+            // 127.0.0.1 stands for localhost and the default port to connect.
+            // Your query,
+            query = "SELECT ID FROM `users` where `Email`='" + login.Text + "' AND `Password`='" + password.Text + "'";
+            // Prepare the connection
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection)
+            {
+                CommandTimeout = 60
+            };
+            MySqlDataReader reader;
+
+            // Let's do it !
+            try {
+                // Open the database
+                databaseConnection.Open();
+
+                // Execute the query
+                reader = commandDatabase.ExecuteReader();
+
+                // All succesfully executed, now do something
+
+                // IMPORTANT : 
+                // If your query returns result, use the following processor :
+
+                
+                if (reader.HasRows) {
+                    databaseConnection.Close();
+                    // Finally close the connection
+                    ActiveTurn();
+                }
+                else
+                {
+                    databaseConnection.Close();
+                }
+
+            }
+            catch (Exception ex) {
+                // Show any error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+        void ActiveTurn()
+        {
+            query = "UPDATE `users` SET `Active` = '1' WHERE `users`.`Email` ='" + login.Text + "'";
+
+            // Prepare the connection
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection)
+            {
+                CommandTimeout = 60
+            };
+            MySqlDataReader reader;
+
+            try
+            {
+                // Open the database
+                databaseConnection.Open();
+                // Execute the query
+                reader = commandDatabase.ExecuteReader();
+                // All succesfully executed, now do something
+                // IMPORTANT : 
+                // If your query returns result, use the following processor 
+                databaseConnection.Close();
+                Main newForm = new Main();
+                newForm.FormClosed += Page_FormClosed;
+                newForm.Show();
+                this.Hide();
+
+            }
+            catch (Exception ex)
+            {
+                // Show any error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Page_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Show(); //отображение 1-й формы после закрытия 2-й
+            this.Close();
+        }
+    }
+}
